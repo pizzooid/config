d0883e1e98d497b89afe8df7935507310f56f775
pietro <pietro@palmesi.com>
2016-03-14 14:27:25 +0100

    using rofi starter

--- archinstall.sh
+++ archinstall.sh
@@ -6,4 +6,4 @@ git clone https://aur.archlinux.org/yaourt.git
 cd yaourt
 makepkg -si
 cd ..
-yaourt -Syu i3-wm i3status dzen2 i3lock xfce4-terminal emacs fasd --noconfirm
+yaourt -Syu i3-wm i3status dzen2 i3lock xfce4-terminal emacs fasd python-pip rofi --noconfirm
