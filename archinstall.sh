ec4c977fb30dad63802d996379886abdb64b58ca
pietro <pietro@palmesi.com>
2016-03-15 12:39:21 +0100

    added vundle

--- archinstall.sh
+++ archinstall.sh
@@ -6,4 +6,4 @@ git clone https://aur.archlinux.org/yaourt.git
 cd yaourt
 makepkg -si --noconfirm
 cd ..
-yaourt -Syu xorg-server xorg-server-utils xorg-apps i3-wm i3status dzen2 i3lock xfce4-terminal emacs fasd python-pip rofi --noconfirm
+yaourt -Syu xorg-server xorg-server-utils xorg-apps arch-wiki-lite i3-wm i3status dzen2 i3lock xfce4-terminal emacs fasd python-pip rofi --noconfirm
